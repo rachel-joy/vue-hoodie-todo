8b0de8f6b42142e88fdcc1c84669fb924cee15c6
rachel-joy <racheljvoth@gmail.com>
2018-03-12 16:58:14 -0700

    Switch login and signup view if logged in user
    Show login and signup forms if user has not logged in. Redirect after a logout.

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,19 +1,23 @@
 <template lang="html">
   <b-container class="content center-text">
-    <b-form @submit.prevent="submit()">
+    <div v-if="user">
+      <p>Hello, {{ user.username }}. You are signed in.</p>
+      <b-btn @click="logout()" type="logout" variant="secondary">Logout</b-btn>
+    </div>
+    <b-form v-else @submit.prevent="submit()">
       <b-form-input type="text" name="username" v-model="credentials.username" placeholder="Username"></b-form-input>
       <b-form-input type="password" name="password" v-model="credentials.password" placeholder="Password"></b-form-input>
       <br>
       <b-button type="submit" variant="secondary">Signup</b-button>
-      <b-btn @click="logout()" type="logout" variant="secondary">Logout</b-btn>
     </b-form>
   </b-container>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
+import Vue from 'vue'
+import Component from 'vue-class-component'
 import hoodie from '../util/hoodie'
+import { User } from '../util/user'
 
 export interface UserCredentials {
   username: string
@@ -23,13 +27,28 @@ export interface UserCredentials {
 @Component
 export default class Signup extends Vue {
   credentials: UserCredentials = {username: '', password: ''}
+  user: User | null = null
+
+  created() {
+    hoodie.account.get().then((user) => {
+      if (user.session) {
+        this.user = user
+      } else {
+        this.user = null
+      }
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
 
   login(credentials: UserCredentials) {
     hoodie.account.signIn(credentials).then(() => {
       return hoodie.account.get()
-    }).then((result) => {
-      console.log(result)
-      this.$router.push('/todo')
+    }).then((user) => {
+      if (user) {
+        this.user = user
+        this.$router.push('/todo')
+      }
     }).catch((err) => {
       console.log(err)
     })
@@ -38,6 +57,7 @@ export default class Signup extends Vue {
   logout() {
     hoodie.account.signOut().then((result) => {
       console.log(result)
+      this.user = null
     }).catch((err) => {
       console.log(err)
     })
